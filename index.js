f2e639600f8f7f1e73189b9523f37892b278f5a6
Swanand Takalkar <takalkarswanand@gmail.com>
2025-09-11 08:40:45 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT}`)
+})
